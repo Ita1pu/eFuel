53c10f8827dbf18ea153cbb3a02a8ed74d60a92c
t.knorr <t.knorr@traeger.de>
2017-03-24 11:10:30 +0100

    Refactored GUI

--- eFuel/eFuel/www/scripts/index.js
+++ eFuel/eFuel/www/scripts/index.js
@@ -8,16 +8,27 @@
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
+        $(document).on("pagecreate", function () {
+            //$(".Panel").panel("open");
+
+            $(".Panel__toggle-btn").click(function () {
+                $(".Panel").panel("toggle")
+            });
+        });
+        var map;
+        var directionsRenderer, directionsService;       
+
+
         // Verarbeiten der Cordova-Pause- und -Fortsetzenereignisse
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
         
         // TODO: Cordova wurde geladen. Führen Sie hier eine Initialisierung aus, die Cordova erfordert.
-        var parentElement = document.getElementById('deviceready');
+        /*var parentElement = document.getElementById('deviceready');
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        receivedElement.setAttribute('style', 'display:block;');*/
 
         cordova.plugins.notification.local.schedule({
             title: "New Message"
@@ -54,6 +65,54 @@
 
     
 })();
+
+function initMap() {
+    var point = {
+        lat: 49.012499,
+        lng: 12.088931
+    };
+    map = new google.maps.Map(document.getElementById("Map__container"), {
+        center: point,
+        zoom: 13,
+        streetViewControl: false,
+        mapTypeControl: false,
+        styles: [{
+            featureType: "poi",
+            elementType: "labels",
+            stylers: [{
+                visibility: "off"
+            }]
+        }, {
+            featureType: "landscape",
+            elementType: "labels",
+            stylers: [{
+                "visibility": "off"
+            }]
+        }]
+    });
+
+    directionsService = new google.maps.DirectionsService();
+    directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map);
+
+    var marker = new google.maps.Marker({
+        position: point,
+        map: map,
+        title: "Point"
+    });
+
+    var request = {
+        origin: point,
+        destination: "Regensburg Ruderzentrum",
+        travelMode: google.maps.TravelMode.WALKING
+    };
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsRenderer.setDirections(result);
+        }
+    });
+
+}
 
 
 
